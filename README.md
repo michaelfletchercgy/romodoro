766567563c500122a7cb529016deb066522f807a
Michael Fletcher <m.fletcher@theplanet.ca>
2019-02-01 12:59:48 -0700

    Initial version.

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # romodoro
+
+Romodoro is a command line Pomodoro timer.
+
+## Setup
+
+ * Install Rust (https://www.rust-lang.org/learn/get-started).
+ * cargo install romodoro
---
+++ src/main.rs
@@ -0,0 +1,127 @@
+extern crate chrono;
+extern crate clap;
+extern crate ctrlc;
+extern crate termion;
+
+use chrono::Duration;
+use chrono::Local;
+
+use clap::App;
+use clap::Arg;
+
+use std::sync::Arc;
+use std::sync::atomic::AtomicBool;
+use std::sync::atomic::Ordering;
+
+use std::thread;
+use std::thread::park_timeout;
+
+fn main() {
+    let matches = App::new("Romodoro")
+                        .version("0.1")
+                        .author("Michael Fletcher <m.fletcher@theplanet.ca>")
+                        .arg(Arg::with_name("goal")
+                               .long("goal")
+                               .help("Display the goal you are trying to achieve.  Helps to keep you focused.")
+                               .takes_value(true))
+                        .get_matches();
+
+    // Setup a CTRL-C handler so we can cleanly close.  This is basically ensuring we reset the colours and cursor.
+    let keep_running_in_handler = Arc::new(AtomicBool::new(true));
+    let keep_running = keep_running_in_handler.clone();
+
+    let current_thread = thread::current();
+    ctrlc::set_handler(move || {
+        keep_running_in_handler.store(false, Ordering::SeqCst);
+        current_thread.unpark();
+    }).unwrap();
+
+    let start = Local::now();
+    let duration = Duration::seconds(60 * 25);
+    let end = start + duration;    
+
+    let (width, height) = termion::terminal_size().unwrap_or((80, 24));
+    
+    println!("{}", termion::clear::All);
+    
+    // Print Goal
+    let goal = matches.value_of("goal");
+
+    if goal.is_some() {
+        let goal_str = &goal.unwrap();
+        println!("{}{}{}{}{}{}", 
+            termion::style::Bold,
+            termion::color::Fg(termion::color::LightRed),
+            termion::cursor::Goto((width / 2) - (goal_str.len() / 2) as u16, height / 2), 
+            &goal.unwrap(),
+            termion::color::Fg(termion::color::Reset),
+            termion::style::Reset);
+    }
+    
+    // Print Start
+    println!("{}{}Start: {}{}", 
+        termion::cursor::Goto(4, 2),
+        termion::color::Fg(termion::color::Reset),
+        termion::color::Fg(termion::color::LightBlue),
+        start.format("%l:%M"),
+        );
+
+    // Print Duration
+    let duration_str_for_size = format!("Duration: {}m", duration.num_minutes());
+    let duration_str = format!(
+        "{}Duration: {}{}m",
+        termion::color::Fg(termion::color::Reset),
+        termion::color::Fg(termion::color::LightBlue),
+        duration.num_minutes()
+        );
+        
+    println!("{}{}", 
+        termion::cursor::Goto((width / 2) - (duration_str_for_size.len() / 2) as u16, 2),
+        duration_str);
+
+    // Print End
+    let end_str = format!(
+        "{}End: {}{}",
+        termion::color::Fg(termion::color::Reset),
+        termion::color::Fg(termion::color::LightBlue),
+        end.format("%l:%M")
+        );
+    println!("{}{}", 
+        termion::cursor::Goto(width - 9 - 4 as u16, 2),
+        end_str);
+    
+    println!("{}", termion::cursor::Hide);
+
+    // Update the screen.
+    while Local::now() < end && keep_running.load(Ordering::SeqCst) {
+        let remaining = end - Local::now();
+
+        if remaining.num_seconds() > 60 { 
+            println!("{}{}Remaining: {}{}m", 
+                termion::cursor::Goto(4, height - 1),
+                termion::color::Fg(termion::color::Reset),
+                termion::color::Fg(termion::color::LightBlue),
+                remaining.num_minutes() + 1);;
+        } else {
+            println!("{}{}Remaining: {}{}s", 
+                termion::cursor::Goto(1, height - 3),
+                termion::color::Fg(termion::color::Reset),
+                termion::color::Fg(termion::color::LightBlue),
+                remaining.num_seconds());;
+        }
+        
+        if remaining.num_seconds() > 120 { 
+            park_timeout(std::time::Duration::from_secs(60));
+        } else {
+            park_timeout(std::time::Duration::from_secs(1));            
+        }
+    }
+
+    // Revert the cursor, colours and style.
+    println!("{}", termion::cursor::Show);
+    println!("{}", termion::color::Fg(termion::color::Reset));
+    println!("{}", termion::style::Reset);
+    
+    
+
+}
